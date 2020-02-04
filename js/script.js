e8ea68a20664b87270fbe17c2c1a773c1df7be34
CWCarlile <59584699+CWCarlile@users.noreply.github.com>
2020-02-03 17:04:43 -0800

    working

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".seven").hover(function(){
+    alert("You give it meaning");
+  },
+  function(){
+    alert("pay attention to what you give attention");
+  }); 
+});
