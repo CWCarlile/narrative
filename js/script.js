2a4426eacc26a5c041587bcff1df43309e764348
CWCarlile <59584699+CWCarlile@users.noreply.github.com>
2020-02-10 14:08:14 -0800

    Ch-Ch-Changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-  $(".seven").hover(function(){
-    alert("You give it meaning");
-  },
-  function(){
-    alert("pay attention to what you give attention");
-  }); 
-});
+
